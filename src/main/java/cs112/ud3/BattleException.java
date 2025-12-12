ed0cd9fb26cccf4837c156454ae356cbf987174f
Adams1234784 <adam.d.szloboda@gmail.com>
2025-12-12 10:09:51 -0800

    Create BattleException.java

---
+++ src/main/java/cs112/ud3/BattleException.java
@@ -0,0 +1,8 @@
+// src/main/java/cs112/ud3/BattleException.java
+package cs112.ud3;
+
+public class BattleException extends Exception {
+    public BattleException(String message) {
+        super(message);
+    }
+}
