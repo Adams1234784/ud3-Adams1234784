29d06b359b31136c430b6113df22e11d01ce41c2
Adams1234784 <adam.d.szloboda@gmail.com>
2025-12-12 10:04:59 -0800

    Create HelloApplication.java

---
+++ src/main/java/cs112/ud3/HelloApplication.java
@@ -0,0 +1,22 @@
+package cs112.ud3;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/cs112/ud3/battle-view.fxml"));
+        Scene scene = new Scene(loader.load(), 700, 520);
+        stage.setTitle("Battle Arena — UD3");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
